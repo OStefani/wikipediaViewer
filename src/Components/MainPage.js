cd861c7ecf4a41ec15af6df497938653eaedabbd
OStefani <ostefanishina@gmail.com>
2019-02-19 19:08:42 +0300

    Add some comments

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,16 +1,18 @@
-import React, {Component, useReducer} from 'react';
+import React, {useReducer} from 'react';
 import DisplayResults from './DisplayReasults';
 import '../styles/MainPage.css';
 import FormComponent from './FormComponent'
 
 const initialInputState = '';
 const results = [];
+
 function reducerInput(state, action) {
     console.log('state: ', state);
     console.log('action.val: ', action.value);
     switch (action.type) {
         case 'text': 
-            return action.value;
+        // If action.type == 'text return action value as inputState
+            return action.value; 
         default:
             throw new Error('Unexpected action');
     }
@@ -24,8 +26,11 @@ function reducerResults(state, action) {
     }
 }
 function MainPage() {
-    
+    // 1. Call useReducer and recieve  state and dispatch
+    /** Reducer returns the current state and dispatch function */
     const [inputState, dispatch] = useReducer(reducerInput, initialInputState); 
+    console.log('inputState: ', inputState);
+
     const handleChange = (e) => {
         return dispatch({type: 'text', value: e.target.value})
     }
@@ -39,27 +44,13 @@ function MainPage() {
         let fullHeight = document.getElementsByClassName('main')[0];
         fullHeight.classList.remove('full-height');
     }
-
-    /*
-    submitForm(e) {
-        e.preventDefault();
-        let query = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrlimit=10&prop=extracts&exintro&explaintext&exsentences=1&exlimit=max&origin=*&gsrsearch=" + this.state.input;
-        console.log('query: ', query);
-        fetch(query)
-            .then(response => response.json())
-            .then(result => this.setState({results: result.query.pages}));
-        let fullHeight = document.getElementsByClassName('main')[0];
-        fullHeight.classList.remove('full-height');
-    }
-  */
         return(
         <div className="main full-height">
             <h1 className="main-title">Wikipedia Viewer</h1>
             <a href="https://en.wikipedia.org/wiki/Special:Random" target="_blank" rel="noopener noreferrer" className="random">Click here to get a random article</a>
+            {/** 2. Call handleChange onChange */}
             <FormComponent handleChange = {handleChange} inputVal = {inputState} submitHandler={submitForm} />
             <DisplayResults pages = {serchResults} />
-            {/*<FormComponent submitHandler = {this.submitForm} handleChange = {this.handleChange} inputVal = {inputState} />  */}          
-            {/*<DisplayResults pages = {this.state.results} />*/}
         </div>
         );
  
